8e5f6f9487fdd53e05d22f079a48c0a55a2864f0
elieyi <elieyioglumujdatqgmail.com>
2022-06-22 10:03:07 +0300

    Product Details Screen

--- lib/controller/dashboard_controller.dart
+++ lib/controller/dashboard_controller.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 class DashboardController extends GetxController {
   var tabIndex = 0;
 
-  void updateIndex(int index){
+  void updateIndex(int index) {
     tabIndex = index;
     update();
   }
